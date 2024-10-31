ffc2f93929ba8d1ff86ca86b201968994b1c6c20
Gonio-NicoletteSairylle-BCSC21A1 <nsairyllegonio@gmail.com>
2024-11-01 03:39:31 +0800

    Add project files.

---
+++ Urban Tide Bus Booking Service Application/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urban_Tide_Bus_Booking_Service_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            // Get the username and password entered by the user
+            string username = textBox1Username.Text;
+            string password = textBox2Password.Text;
+
+            // Check if the username and password are correct
+            if (username == "user" && password == "12345")
+            {
+                // If correct, open the main menu form
+                Form2MainMenu mainMenuForm = new Form2MainMenu();
+                mainMenuForm.Show();
+                this.Hide(); // Hide the login form
+            }
+            else
+            {
+                // Display an error message if the login fails
+                MessageBox.Show("Incorrect username or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void buttonLoginGuest_Click(object sender, EventArgs e)
+        {
+            // Guest login functionality
+            string username = textBox1Username.Text;
+            string password = textBox2Password.Text;
+
+            if (username == "name" && password == "password")
+            {
+                Form3 guestForm = new Form3();
+                guestForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or password for guest login. Please try again.", "Guest Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+   
---
+++ Urban Tide Bus Booking Service Application/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urban_Tide_Bus_Booking_Service_Application
+{
+    public partial class Form2MainMenu : Form
+    {
+        public Form2MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Show Form1 and hide Form3
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void button1book_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2see_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3routes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4schedules_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
